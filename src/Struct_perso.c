62ce5fc6f112bcadc9f0d1451c2c7d952c437fd5
oussamablk-1 <78028622+oussamablk-1@users.noreply.github.com>
2021-02-09 14:40:07 +0100

    Add files via upload

---
+++ src/Struct_perso.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum{
+    Bill,
+    Jane,
+    Pau,
+    Ennemie
+}t_categ;
+
+typedef enum{
+    sniper,
+    assaut,
+    corps,
+    vilain
+}t_type;
+
+typedef struct{
+    int pos_X;
+    int pos_Y
+    int attaque;
+    int defense;
+    int HP;
+    int PA;
+    int PM;
+    t_categ categ;
+    t_type type;
+}t_pers;
+
+typedef struct{
+    int ATT;
+    int DEF;
+    int HP_obj;
+    int PA_obj;
+    int PM obj;
+}t_objet;
+
+
+
+void afficher_perso_jouant(t_pers* pers, int num){
+    printf ("Nom : %s\n",pers[num].categ);
+    printf ("Type du personnage : %s\n",pers[num].type)
+    printf ("Position : %i %i\n",pers[num].pos_x , pers[num].pos_y);
+    printf ("Point de vie : %i\n",pers[num].HP);
+    printf ("Attaque : %i\n",pers[num].attaque);
+    printf ("Défense : %i\n",pers[num].defense);
+    printf ("Point d'action : %i\n",pers[num].PA);
+    printf ("Point de mouvement : %i\n",pers[num].PM);
+    printf ("\n");
+    printf ("\n");
+    printf ("\n");
+}
+
+
+void objet_stat(t_pers* pers, t_objet* obj, char* objet, int num){
+    if (objet == 'sniper'){
+        if (pers[num].type == 'sniper'){
+            pers[num].attaque += obj[/*objet*/].ATT;
+            pers[num].PA += obj[/*objet*/].PA_obj;
+            pers[num].PM += obj[/*objet*/].PM_obj;
+        }
+        if (pers[num].type /= 'sniper'){
+            printf("La classe du personnage n'est pas compatible avec l'arme\n");
+        }
+    }
+
+    if (objet == 'assaut'){
+        if (pers[num].type == 'assaut'){
+            pers[num].attaque += obj[/*objet*/].ATT;
+            pers[num].PA += obj[/*objet*/].PA_obj;
+            pers[num].PM += obj[/*objet*/].PM_obj;
+        }
+        if (pers[num].type /= 'assaut'){
+            printf("La classe du personnage n'est pas compatible avec l'arme\n");
+        }
+    }
+
+    if (objet == 'corps'){
+        if (pers[num].type == 'corps'){
+            pers[num].attaque += obj[/*objet*/].ATT;
+            pers[num].PA += obj[/*objet*/].PA_obj;
+            pers[num].PM += obj[/*objet*/].PM_obj;
+        }
+        if (pers[num].type /= 'corps'){
+            printf("La classe du personnage n'est pas compatible avec l'arme\n");
+        }
+    }
+}
+
+
