a625cb7297536557595a178ff99416806dbfd90f
gdec-xyz <gaspard.decoux.etu@univ-lemans.fr>
2021-02-19 15:58:05 +0100

    Premier niveau et définitions pour les niveaux

---
+++ src/niveaux.c
@@ -0,0 +1,9 @@
+/**
+ * \file niveaux.c
+ * \brief Fonctions pour la gestion de niveaux à partir de leurs fichiers
+ *
+ * Les niveaux sont représentés dans leurs fichiers media/niveaux/XX.niv où XX est le numéro du niveau.
+ * Chaque fichier est une suite de caractères de chiffres sans espaces, représentant la matrice de gauche à droite et de haut en bas.
+ * Chaque caractère représente un bloc spécifique
+ * \sa niveaux.h
+ */
