9ec7335e4ff412cf2530b1414193d64df6ba2011
oussamablk-1 <78028622+oussamablk-1@users.noreply.github.com>
2021-04-18 17:57:24 +0200

    Update labyrinthe_niveau.c

--- src/labyrinthe_niveau.c
+++ src/labyrinthe_niveau.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <time.h>
 #include "../lib/labyrinthe_niveau.h"
 #include "../lib/commun.h"
 
@@ -22,7 +20,6 @@ Lab_Room *rand_room(Room m[MAX_ROWS][MAX_COL], Lab_Room *L, int n) {
 
 	do {
 		  i=random() % n;
-		  //printf("rand_room: %d\n",i);
 		  j=0;
 		  r=L;
 		  while (j<i){
@@ -56,31 +53,28 @@ Lab_Room *add_door(Room m[MAX_ROWS][MAX_COL], Lab_Room *r) {
 	y2=y;
 
     do {
-      i=random() % 4; // Random door creation
-    } while (m[x][y].doors[i]); // repeat if already existing
+      i=random() % 4; //création d'une pièce alétoirement
+    } while (m[x][y].doors[i]); // recommencer si elle existe deja
 
 
-    // Find the room on the other side
-	if (i==0)      { y2++; } // North 
+    //Ttrouver la pièce de l'autre coté
+	if (i==0)      { y2++; } // Nord 
 	else if (i==1) { x2++; } // Est 
-	else if (i==2) { y2--; } // South 
-	else if (i==3) { x2--; } // West 
+	else if (i==2) { y2--; } // Sud 
+	else if (i==3) { x2--; } // Ouest 
 
-    j=(i+2)%4; // Find the opposite door on the other room
+    j=(i+2)%4; // Trouver la porte opposé dans la pièce
 	m[x][y].doors[i]=1; m[x2][y2].doors[j]=1;
-	//printf("Add door %d, on Room(%d,%d)\n",i,x,y);
-	//printf("Add door %d, on Room(%d,%d)\n",j,x2,y2);
 	m[x][y].open_doors++;
 	m[x2][y2].open_doors++;
 	
-	if (m[x2][y2].open_doors==1) { // First door in that room
-		//printf("Create Room(%d,%d)\n",x2,y2);
+	if (m[x2][y2].open_doors==1) { // Premièere porte dans la pièce
 		new_room = (Lab_Room *)malloc(sizeof(Lab_Room));
 		new_room->pos_x=x2;
 		new_room->pos_y=y2;
 	}
 
-	return(new_room); // return new room created or NULL
+	return(new_room); // retourne la pièece créer ou NULL si elle n'a pas été créer
 }
 
 /**
@@ -103,44 +97,5 @@ void print_lab(Room m[MAX_ROWS][MAX_COL], Lab_Room *r) {
 	  printf ("\n");
 	  r=r->next;
 	} while (r!=NULL);
-
-}
-
-
-int main(int argc, char *argv[]) {
-
-  Room Map[MAX_ROWS][MAX_COL];   
-  Lab_Room *Lab, *ri, *nr;
-  int nb_rooms=13,nb_current_rooms=1;
-  struct timeval t;
-
-  gettimeofday(&t,0);
-  srandom(t.tv_usec);
-
-  memset(Map,0,MAX_ROWS*MAX_COL*sizeof(Room));
-
-  if (argc > 1) { nb_rooms = atoi(argv[1]); } // check nb_rooms < sqrt(MAX_ROWS)
-
-  //printf("nb rooms = %d\n",nb_rooms);
- 
-  Lab = (Lab_Room *)malloc(sizeof(Lab_Room));
-  Lab->pos_x=nb_rooms;
-  Lab->pos_y=nb_rooms;
-  Lab->next=NULL;
-
-  while (nb_current_rooms < nb_rooms) {
-	ri=rand_room(Map,Lab,nb_current_rooms);
-	nr=add_door(Map,ri);
-	if (nr!=NULL) {
-		//printf("Adding new Room(%d,%d) to Room list\n",nr->pos_x,nr->pos_y);
-		nr->next=Lab;
-		Lab=nr;
-    	nb_current_rooms++;
-	}
-  }	
-
-  print_lab(Map,Lab);
-
-  return 0;
 
 }
